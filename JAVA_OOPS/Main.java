2854528648a268291e02ab83c37434bdc369b18c
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-03-30 16:55:00 +0530

    Update Main.java

--- JAVA_OOPS/Main.java
+++ JAVA_OOPS/Main.java
@@ -6,6 +6,7 @@ public class Main {
         cookieOne.setColor("yellow");
         System.out.println(cookieOne.getColor());
         Cookie cookieTwo=new Cookie("Blue");
+        cookieTwo.setColor("black");
         System.out.println(cookieTwo.getColor());
 
         
