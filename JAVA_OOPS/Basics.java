bf6dd39e37c6096a296d3231e8842dc07d6d2d23
NirajanDevHub <160125373+nirajanDevHub108@users.noreply.github.com>
2025-03-30 13:11:13 +0530

    basic java class

---
+++ JAVA_OOPS/Basics.java
@@ -0,0 +1,13 @@
+class test{
+    public static void PrintTest() {
+        System.out.println("Hello, World!");
+    }
+}
+
+
+
+public class Basics {
+    public static void main(String[] args) {
+        test.PrintTest();
+    }
+}
